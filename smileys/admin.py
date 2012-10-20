4a4ac15fdb68a5d593851776eae945d074e9fefc
steve <steve@steve-K73SD>
2012-10-20 12:12:53 +0200

    Several improvements, tags field in models.

--- smileys/admin.py
+++ smileys/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from smileys.models import Smiley
+from django.utils.translation import ugettext_lazy as _
+from easy_thumbnails.files import get_thumbnailer
 
 class SmileyAdmin(admin.ModelAdmin):
-    list_display = ('pattern', 'description', 'image', 'is_regex', 'is_active')
-    list_fileter = ('is_regex', 'is_active')
-    search_fields = ('pattern', 'description', 'image')
+    list_select_related = True
+    list_display = ['id', 'pattern', 'description', 'get_icon_link', 'image', 'is_regex', 'is_active']
+    list_filter = ['is_active', 'is_regex']
+    readonly_fields = []
+    search_fields = ['description', 'tags']
+    #---------------------------------- Récupérer un markup de l'image avec lien
+    def get_icon_link(self, obj):
+        if obj.image:
+            thumbnailer = get_thumbnailer(obj.image.name)
+            thumbnail_options = {'crop': True, 'size':(32,32)}
+            result = thumbnailer.get_thumbnail(thumbnail_options)
+            return "<img src='%s'>" % (obj.image.url)
+        else:
+            return u"<em>%s</em>" % _(u"None")
+    get_icon_link.allow_tags = True
+    get_icon_link.short_description = _(u"Image")
 
 admin.site.register(Smiley, SmileyAdmin)
--- smileys/models.py
+++ smileys/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 class SmileyManager(models.Manager):
     def active(self):
-        "Retrieves all active smiley codes!"
         return self.get_query_set().filter(is_active=True)
 
 class Smiley(models.Model):
-    pattern = models.CharField(max_length=50)
-    description = models.CharField(max_length=100, blank=True)
-    image = models.ImageField(upload_to='smileys')
-    is_regex = models.BooleanField(blank=True)
-    is_active = models.BooleanField(default=True, blank=True)
+    pattern = models.CharField(max_length=24, verbose_name=_("Pattern"))
+    description = models.CharField(max_length=128, blank=True, verbose_name=_("Description"))
+    image = models.ImageField(upload_to=getattr(settings, 'SMILEYS_PATH', 'smileys'), verbose_name=_("Image"))
+    is_regex = models.BooleanField(blank=True, verbose_name=_("Is a regex"))
+    is_active = models.BooleanField(default=True, blank=True, verbose_name=_("Active"))
+    tags = models.CharField(max_length=64, blank=True, verbose_name=_("Tags"))
 
     def __unicode__(self):
         return self.description or self.pattern
 
     class Meta:
         ordering = ('description', 'pattern')
+        verbose_name = _(u"Smiley")
+        verbose_name_plural = _(u"Smileys")
